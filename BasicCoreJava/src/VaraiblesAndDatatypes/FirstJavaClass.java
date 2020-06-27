7578bf57a9c4a74201a839e5f51261275010ef0e
AjinkyaIsankar <ajinkyaisankar@gmail.com>
2020-06-27 13:40:55 +0530

    First java Project

---
+++ BasicCoreJava/src/VaraiblesAndDatatypes/FirstJavaClass.java
@@ -0,0 +1,11 @@
+package VaraiblesAndDatatypes;
+
+public class FirstJavaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Automation");
+
+	}
+
+}
